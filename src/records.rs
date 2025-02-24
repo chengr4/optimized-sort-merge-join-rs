d70eb5291ac3f3a9b7257ce1dccf080bd26d8bdf
R4 Cheng <karaburi2023@gmail.com>
2025-02-23 19:37:19 -0800

    Add function to grab department records from input

--- src/records.rs
+++ src/records.rs
@@ -49,3 +49,28 @@ pub fn grab_emp_record<T: BufRead>( empin: &mut T) -> Records {
         rec
 
 }
+
+pub fn grab_dept_record<T: BufRead>( deptin: &mut T) -> Records {
+    let mut line = String::new();
+    if let Ok(bytes) = deptin.read_line(&mut line) {
+        if bytes > 0 {
+            let parts: Vec<&str> = line.trim_end().split(',').collect();
+            if parts.len() == 3 {
+                let did = parts[0].parse::<i32>().unwrap_or_default();
+                let dname = parts[1].to_string();
+                let manager_id = parts[2].parse::<i32>().unwrap_or_default();
+                return Records {
+                    dept_record: DeptRecord {
+                        did,
+                        dname,
+                        manager_id,
+                    },
+                    ..Default::default()
+                };
+            }
+        }
+    }
+    let mut rec = Records::default();
+        rec.no_values = -1;
+        rec
+}
